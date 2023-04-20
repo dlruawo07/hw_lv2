467584702e14025679a05933a3cb0a9874e98e9f
dlruawo07 <dlruawo07@gmail.com>
2023-04-20 19:04:22 +0900

    added swagger

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output");
+
 const app = express();
 const port = 3000;
 
@@ -12,6 +15,7 @@ const connect = require("./schemas");
 
 connect();
 
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 app.use(cookieParser());
 
